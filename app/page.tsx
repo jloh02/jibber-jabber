6643e12e54b9b8770322bc41fbfc98b47078c898
Jonathan Loh <jonathanloh02@gmail.com>
2023-05-26 15:33:12 +0800

    Add GitHub icon

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Login from "./login";
 import styles from "./page.module.css";
 
@@ -11,15 +12,27 @@ export default function Home() {
       <div className={styles.footer}>
         <p>
           Jibber Jabber is a chat application that enables you to interact with
-          other Riot Games players.
+          other Riot Games players
         </p>
-        <p>Riot Games is not affiliated with this project.</p>
-        <a href="https://github.com/jloh02/jibber-jabber/issues ">
+        <p>Riot Games is NOT affiliated with this project</p>
+        <a
+          href="https://github.com/jloh02/jibber-jabber/issues"
+          target="_blank"
+        >
           Report a bug or request a feature
         </a>
-        {/* <a href="github.com/jloh02/repo/issues ">
-          <img src="\src\assets\git.png" alt="View source or report issue" />
-        </a> */}
+        <a
+          className={styles.githubIcon}
+          href="https://github.com/jloh02/jibber-jabber"
+          target="_blank"
+        >
+          <Image
+            src="/github.png"
+            width={45}
+            height={45}
+            alt="View source or report issue"
+          />
+        </a>
       </div>
     </>
   );
